ba583856131545f7ee298aab4320a0d7ab7721e5
Dario Olazar <lernny@gmail.com>
2025-11-28 17:47:08 -0300

    Improve the keymaps

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -8,4 +8,5 @@ return {
     local new_config = not vim.diagnostic.config().virtual_lines
     vim.diagnostic.config { virtual_lines = new_config }
   end, { desc = 'Toggle diagnostic virtual_lines' }),
+  vim.keymap.set('n', ';', ':', { noremap = true }),
 }
